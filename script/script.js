26e5fa80b499e3f2832cd0c58c5b5c4839b9b08b
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2020-11-12 12:08:46 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,11 @@
-const send = document.getElementById("contact-send");
-
-send.addEventListener("click", notify);
-
-function notify() {
-    alert("Because of a lot of spam the contact form is temporarily not in use. Please contact your personal customer care representative");
-}
-
+const emailaddress = document.getElementById("emailaddress");
 
 // Email obfuscator script 2.1 by Tim Williams, University of Arizona
 // Random encryption key feature coded by Andrew Moulden
 // This code is freeware provided these four comment lines remain intact
 // A wizard to generate this code is at http://www.jottings.com/obfuscator/
-{ coded = "xbdbtpiipI8-J5bhAaWdb@FmtAA.JAa"
-  key = "Q5BZKT0yvAX8ULOJp76SmMdnNhbf3RWGuPlVqkF2o1gDzwciHCYajrI4tsxE9e"
+{ coded = "7AiAYryyr5C-9zAmMtqiA@p6YMM.9Mt"
+  key = "CxmDzjHBho15FqKv8fPJpwc34idA0OagLtTuSVlYMGnQeW2X7ZEyk9RbrIsNU6"
   shift=coded.length
   link=""
   for (i=0; i<coded.length; i++) {
@@ -25,5 +18,17 @@ function notify() {
       link += (key.charAt(ltr))
     }
   }
-document.getElementById("email").innerHTML = `<a href='mailto:"+link+"'>"+link+"</a>"`;
+emailaddress.innerHTML = "<a href='mailto:"+link+"'>Email</a>"
 }
+
+
+
+
+const send = document.getElementById("contact-send");
+
+send.addEventListener("click", notify);
+
+function notify() {
+    alert("Because of a lot of spam the contact form is temporarily not in use. Please contact your personal customer care representative");
+}
+
