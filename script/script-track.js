38e17d647cad953a2365f5a06b9ff94759fc9b6a
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2020-11-16 19:58:12 +0100

    Update script-track.js

--- script/script-track.js
+++ script/script-track.js
@@ -4,7 +4,6 @@ const track = document.getElementById("trackbutton");
 track.addEventListener("click", trackParcel);
 
 function trackParcel() {
-    console.log("click");
     let trackingnumber = document.getElementById("trackingnumber").value;
     console.log(trackingnumber);
     window.location=`http://fsr-shipping.netlify.app/trk/${trackingnumber}.html`;
