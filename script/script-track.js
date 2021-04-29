b74e50d9c0abd84b2c0ca577731ecd922f8597b3
fsrshipping <74324252+fsrshipping@users.noreply.github.com>
2021-04-29 22:14:54 +0300

    Add files via upload

--- script/script-track.js
+++ script/script-track.js
@@ -1,20 +1,33 @@
 $(document).ready(function() {
+
+    function checkPage(link) {
+        let request = new XMLHttpRequest();  
+        request.open('GET', link, true);
+        request.onreadystatechange = function(){
+            if (request.readyState === 4){
+                if (request.status === 404) { 
+                    $("#tracking-main").load("trk/404.html"); 
+                }  
+            }
+        };
+        request.send();
+    }
+
     $("#trackbutton").click(function() {
         let getNumber = $("#trackingnumber").val();
-        let link = "https://fsr-shipping.netlify.app/trk/" + getNumber + ".html"
+        let link = "https://fsr-shipping.netlify.app/trk/" + getNumber + ".html";
+        checkPage(link);      
         $("#tracking-main").load(link);
     });
+    
 
     $('#trackingnumber').on('keypress', function (e) {
         if(e.which === 13){
             let getNumber = $("#trackingnumber").val();
-            let link = "https://fsr-shipping.netlify.app/trk/" + getNumber + ".html"
+            let link = "https://fsr-shipping.netlify.app/trk/" + getNumber + ".html";
+            checkPage(link)
             $("#tracking-main").load(link);
-           
         }
     });
 
 });
-
-
-  
